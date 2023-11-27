bf531f0b49697cc24d396638477e1bea95412e68
Asilbek Muminov <asilbekmuminov@Asilbeks-MacBook-Air.local>
2023-11-27 01:37:08 +0100

    feat: finaly connected to remote cloud DB

--- src/services/database.ts
+++ src/services/database.ts
@@ -3,8 +3,12 @@ import { DBconfig } from "../config/db.config";
 
 async function connectToDatabase() {
   try {
-    const pool = mysql.createPool(DBconfig);
-
+    const pool = mysql.createPool({
+      host: DBconfig.MYSQL_ADDON_HOST,
+      user: DBconfig.MYSQL_ADDON_USER,
+      password: DBconfig.MYSQL_ADDON_PASSWORD,
+      database: DBconfig.MYSQL_ADDON_DB
+    });
     const connection = await pool.getConnection();
     console.log('Connected to MySQL database!');
     connection.release();
@@ -16,3 +20,20 @@ async function connectToDatabase() {
 }
 
 export default connectToDatabase;
+
+//     const DATABASE_URL='mysql://u8cg5p90hd50m9nt:7CCXVFjbEAGwFb7bermF@bhobuo3elequsu8qehbi-mysql.services.clever-cloud.com:3306/bhobuo3elequsu8qehbi'
+
+
+// async function connectToDatabaseWithURI() {
+//   try {
+//     const connection = await mysql.createConnection(DATABASE_URL);
+//     console.log('Connected to MySQL database! with uri');
+//     connection.end(); // Close the connection
+//     return connection;
+//   } catch (error) {
+//     console.error('Error connecting to database: uri connection failed too', error);
+//     throw new Error('Database connection failed uri connection failed too');
+//   }
+// }
+
+// export default connectToDatabaseWithURI;
